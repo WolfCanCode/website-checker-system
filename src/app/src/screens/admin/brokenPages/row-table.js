0ff8452bf54aaf855ad39a7a0934995162aebe48
ngoct <wolfcancode@gmail.com>
2018-10-15 14:08:05 +0700

    add button ui disabled

--- src/app/src/screens/admin/brokenPages/row-table.js
+++ src/app/src/screens/admin/brokenPages/row-table.js
@@ -6,7 +6,7 @@ export default class TableRow extends Component {
     render() {
         return (<Table.Row>
 
-            <Table.Cell style={{ width: '100px', whiteSpace: 'normal', wordBreak: 'break-all' }}><a href={this.props.urlPage}>{this.props.urlPage}</a></Table.Cell>
+            <Table.Cell style={{ width: '100px', whiteSpace: 'normal', wordBreak: 'break-all' }}><a href={this.props.urlPage}>{this.props.urlPage.split("https://www.")[1]}</a></Table.Cell>
             <Table.Cell style={{ width: '100px', whiteSpace: 'normal', wordBreak: 'break-all' }}><Label style={{ fontSize: '13px' }} color='red' horizontal>{this.props.stt}</Label></Table.Cell>
             <Table.Cell style={{ width: '100px', whiteSpace: 'normal', wordBreak: 'break-all' }}><Label style={{ fontSize: '13px' }} >{this.props.httpCode}</Label></Table.Cell>
 
