b2779a8c7b29dec6a374fbf087c3d600f819a0c4
mptt2 <anhtp1608@gmail.com>
2018-10-18 20:36:09 +0700

    Update UI Contact Detail

--- src/app/src/screens/admin/contact/index.js
+++ src/app/src/screens/admin/contact/index.js
@@ -1,25 +1,129 @@
 import React, { Component } from 'react';
-import {Segment, Button, Table, Icon } from 'semantic-ui-react'
+import {Segment, Button, Table, Icon} from 'semantic-ui-react'
+import TableRow from './row-table';
 export default class Contact extends Component {
+  state = { list: [], loadingTable: false, isDisable: false, countPhone: 0, countEmail: 0 };
 
+
+  componentDidMount() {
+      var comp = [];
+      
+      this.setState({ loadingTable: true });
+      var param = [{ "url": "https://twitter.com/hashtag/hiccupsteahouse?lang=en" },
+      { "url": "https://www.facebook.com/hiccupsteahouse" },
+      { "url": "https://www.instagram.com/hiccupsteahouse/" },
+      { "url": "http://hiccupsteahouse.com/" },
+      { "url": "https://www.orderhiccupsteahouse.com/" },
+      { "url": "http://hiccupsteahouse.com/wp-content/uploads/Hiccups-TeaHouse-Menu-9-18.pdf" },
+      { "url": "http://hiccupsteahouse.com/hiccups-locations/" },
+      { "url": "http://hiccupsteahouse.com/contact-us/" },
+      { "url": "http://hiccupsteahouse.com/careers/" },
+      { "url": "http://www.churroholic.com/" },
+      ];
+      fetch("/api/contactDetail/lastest", {
+          method: 'POST',
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(param)
+      }).then(response => response.json()).then((data) => {
+          comp = data.map((item, index) => {
+              return (<TableRow key={index} phoneMail={item.phoneMail} url={item.url} type={item.type} />);
+          });
+          let countP = 0;
+         data.map((item) => {
+            if(item.type==='Phone'){ 
+                countP++;
+            }
+            return countP;
+        });
+          let countE=0;
+        data.map((item)=>{
+          if(item.type==='Mail'){
+            countE++;
+          }
+          return countE;
+        })
+        
+        
+         this.setState({countEmail:countE})
+          this.setState({countPhone:countP})
+          this.setState({ list: comp });
+          this.setState({ loadingTable: false });
+      });
+
+
+  }
+  _doContactDetailTest() {
+      this.setState({ loadingTable: true, isDisable: true });
+      var comp = [];
+      var param = [{ "url": "https://twitter.com/hashtag/hiccupsteahouse?lang=en" },
+      { "url": "https://www.facebook.com/hiccupsteahouse" },
+      { "url": "https://www.instagram.com/hiccupsteahouse/" },
+      { "url": "http://hiccupsteahouse.com/" },
+      { "url": "https://www.orderhiccupsteahouse.com/" },
+      { "url": "http://hiccupsteahouse.com/wp-content/uploads/Hiccups-TeaHouse-Menu-9-18.pdf" },
+      { "url": "http://hiccupsteahouse.com/hiccups-locations/" },
+      { "url": "http://hiccupsteahouse.com/contact-us/" },
+      { "url": "http://hiccupsteahouse.com/careers/" },
+      { "url": "http://www.churroholic.com/" },
+      ];
+      fetch("/api/contactDetail", {
+          method: 'POST',
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(param)
+      }).then(response => response.json()).then((data) => {
+          comp = data.map((item, index) => {
+              return (<TableRow key={index} phoneMail={item.phoneMail} url={item.url} type={item.type} />);
+          });
+          let countP = 0;
+          data.map((item) => {
+             if(item.type==='Phone'){ 
+                 countP++;
+             }
+             return countP;
+         });
+           let countE=0;
+         data.map((item)=>{
+           if(item.type==='Mail'){
+             countE++;
+           }
+           return countE;
+         })
+         
+         
+          this.setState({countEmail:countE})
+           this.setState({countPhone:countP})
+          this.setState({ list: comp });
+          this.setState({ loadingTable: false });
+      });
+  }
   render() {
     return (
       <div >
           <Segment.Group horizontal  style={{margin:0}} >
             <Segment basic>
+            <Button icon labelPosition='right' disabled={this.state.isDisable} onClick={() => this._doContactDetailTest()}>
+                        Check
+                       <Icon name='right arrow' />
+                    </Button>
               <Segment.Group horizontal>
                 <Segment style={{ margin: 'auto', textAlign: 'center', padding: 0 }}>
                   <Icon name='phone square' size='huge' />
                 </Segment>
                 <Segment>
-                  <p style={{ fontSize: 24 }}>2 <br />
+                  <p style={{ fontSize: 24 }}>{isNaN(this.state.countPhone)?0:(this.state.countPhone)} <br />
                     Phone Numbers</p>
                 </Segment>
                 <Segment style={{ margin: 'auto', textAlign: 'center', padding: 0 }}>
                   <Icon name="mail" size='huge' />
                 </Segment>
                 <Segment>
-                  <p style={{ fontSize: 24 }}>2 <br /> Emails</p>
+                  <p style={{ fontSize: 24 }}>{isNaN(this.state.countEmail)?0:(this.state.countEmail)}<br /> Emails</p>
                 </Segment>
               </Segment.Group>
               <Button style={{ marginBottom: '10px' }} floated='right'><Icon name="print" />Export</Button>
@@ -29,24 +133,13 @@ export default class Contact extends Component {
                     <Table.HeaderCell>Phone</Table.HeaderCell>
                     <Table.HeaderCell>Page affected</Table.HeaderCell>
                     <Table.HeaderCell>Action</Table.HeaderCell>
-
                   </Table.Row>
                 </Table.Header>
                 <Table.Body>
-                  <Table.Row>
-                    <Table.Cell style={{ width: '100px', whiteSpace: 'normal', wordBreak: 'break-all' }}>877-925-7140</Table.Cell>
-                    <Table.Cell style={{ width: '100px', whiteSpace: 'normal', wordBreak: 'break-all' }}> 3 <Icon name="clone" /></Table.Cell>
-                    <Table.Cell style={{ width: '100px', whiteSpace: 'normal', wordBreak: 'break-all' }}><Button>Edit</Button></Table.Cell>
-
-                  </Table.Row>
-                  <Table.Row>
-                    <Table.Cell style={{ width: '100px', whiteSpace: 'normal', wordBreak: 'break-all' }}>877-925-7140</Table.Cell>
-                    <Table.Cell style={{ width: '100px', whiteSpace: 'normal', wordBreak: 'break-all' }}> 2  <Icon name="clone" /></Table.Cell>
-                    <Table.Cell style={{ width: '100px', whiteSpace: 'normal', wordBreak: 'break-all' }}><Button>Edit</Button></Table.Cell>
-                  </Table.Row>
+                  {this.state.list}
                 </Table.Body>
               </Table>
-              <Table singleLine>
+              {/* <Table singleLine>
                 <Table.Header>
                   <Table.Row>
                     <Table.HeaderCell>Email</Table.HeaderCell>
@@ -67,7 +160,7 @@ export default class Contact extends Component {
                     <Table.Cell style={{ width: '100px', whiteSpace: 'normal', wordBreak: 'break-all' }}><Button>Edit</Button></Table.Cell>
                   </Table.Row>
                 </Table.Body>
-              </Table>
+              </Table> */}
 
             </Segment>
             {/* <Segment basic>
