523ff69922e4220d5c0c71dc52d152ea4f36732f
ngoct <wolfcancode@gmail.com>
2018-10-19 10:39:17 +0700

    add Error page

---
+++ src/app/src/screens/admin/error404/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+
+
+
+
+
+
+
+class Error404 extends Component {
+
+    render() {
+        return (
+            <div>
+               404 the page cannot found
+            </div>
+
+        );
+    }
+
+
+
+}
+
+export default Error404;
