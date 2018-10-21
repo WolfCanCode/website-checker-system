31c6f9c815eb5ff270e41505ce50103a4b6afc11
vinguyen1996 <vii.nguyen18@gmail.com>
2018-10-21 17:16:25 +0700

    Update Backend: Sitemap Controller
    Commit Sitemap Controller

--- src/src/main/java/com/fpt/capstone/wcs/controller/SitemapController.java
+++ src/src/main/java/com/fpt/capstone/wcs/controller/SitemapController.java
@@ -1,27 +1,31 @@
 package com.fpt.capstone.wcs.controller;
 
-import com.fpt.capstone.wcs.model.SiteLink;
-import com.fpt.capstone.wcs.model.SiteMap;
+import com.fpt.capstone.wcs.service.SiteMapService;
+import com.fpt.capstone.wcs.model.SiteMapOutput;
+import com.fpt.capstone.wcs.model.Url;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class SitemapController {
 
-    @GetMapping("api/sitemap")
-    public List<String> buildSiteMap(@RequestParam("site") String url) {
-        SiteMap sitemap = new SiteMap(url);
+    @PostMapping("/api/sitemap")
+    public List<SiteMapOutput> getSiteMap(@RequestBody Url url) throws MalformedURLException {
+        String urlRoot = url.getUrl();
+        System.out.println("ROOT: " + urlRoot);
+        SiteMapService sitemap = new SiteMapService(urlRoot);
         sitemap.buildSiteMap();
         List<String> rs = sitemap.getDecodeGraph();
-        return rs;
+        SiteMapOutput sm = new SiteMapOutput();
+        sm.setMap(rs.get(0));
+        sm.setTypeMap(rs.get(1));
+        sm.setUrlMap(rs.get(2));
+        List<SiteMapOutput> res = new ArrayList<>();
+        res.add(sm);
+        return res;
     }
 
-    @PostMapping("/api/run")
-    public List<String> run(@RequestBody String url) {
-        return Arrays.asList("a", "b");
-    }
-
-//    public List<List<SiteLink>>
 }
