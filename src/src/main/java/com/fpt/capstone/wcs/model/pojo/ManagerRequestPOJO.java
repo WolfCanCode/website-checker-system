7babcea069859122490edf8f84c333f65b378391
dtnguyen1316 <32295626+dtnguyen1316@users.noreply.github.com>
2018-11-22 15:24:34 +0700

    Remove word entity and repository

--- src/src/main/java/com/fpt/capstone/wcs/model/pojo/ManagerRequestPOJO.java
+++ src/src/main/java/com/fpt/capstone/wcs/model/pojo/ManagerRequestPOJO.java
@@ -3,7 +3,6 @@ package com.fpt.capstone.wcs.model.pojo;
 import com.fpt.capstone.wcs.model.entity.User;
 import com.fpt.capstone.wcs.model.entity.WarningWord;
 import com.fpt.capstone.wcs.model.entity.Website;
-import com.fpt.capstone.wcs.model.entity.Word;
 
 import lombok.Data;
 import lombok.Getter;
--- src/src/main/java/com/fpt/capstone/wcs/repository/WarningWordRepository.java
+++ src/src/main/java/com/fpt/capstone/wcs/repository/WarningWordRepository.java
@@ -2,7 +2,6 @@ package com.fpt.capstone.wcs.repository;
 
 import com.fpt.capstone.wcs.model.entity.User;
 import com.fpt.capstone.wcs.model.entity.WarningWord;
-import com.fpt.capstone.wcs.model.entity.Word;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
